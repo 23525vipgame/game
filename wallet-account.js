60c43f0cbb75a8d4d7194cfb45c62a71fc4e4399
23525 <tuanguyen130287@gmail.com>
2026-08-26 20:58:16 +0700

    Update WALLET_QR_API_URL to new endpoint

--- wallet-account.js
+++ wallet-account.js
@@ -4,7 +4,7 @@
  * N\u1ea1p SAU app.js.
  */
 const WALLET_QR_API_URL=
-  'https://arrives-terrorists-seminars-webmaster.trycloudflare.com/run-bot';
+  'https://extensive-investigator-elder-geo.trycloudflare.com/run-bot';
 const MIN_DEPOSIT_AMOUNT=50000;
 let walletDepositTimer=null;
 let walletQrObjectUrl='';
