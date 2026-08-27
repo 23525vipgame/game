89763837678b92bdd3bf3984256fa11658d54393
23525 <tuanguyen130287@gmail.com>
2026-08-27 21:44:28 +0700

    Update wallet-account.js

--- wallet-account.js
+++ wallet-account.js
@@ -4,7 +4,7 @@
  * N\u1ea1p SAU app.js.
  */
 const WALLET_QR_API_URL=
-  'https://cleaner-specification-developmental-copyright.trycloudflare.com/run-bot';
+  'https://awarded-fitness-ribbon-longitude.trycloudflare.com/run-bot';
 const MIN_DEPOSIT_AMOUNT=50000;
 let walletDepositTimer=null;
 let walletQrObjectUrl='';
