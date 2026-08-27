61be1847ae43675716d34000a7c86e41dcd04bcf
23525 <tuanguyen130287@gmail.com>
2026-08-27 16:07:39 +0700

    Update wallet-account.js

--- wallet-account.js
+++ wallet-account.js
@@ -4,7 +4,7 @@
  * N\u1ea1p SAU app.js.
  */
 const WALLET_QR_API_URL=
-  'https://surfing-harry-assumed-reviewed.trycloudflare.com/run-bot';
+  'https://cleaner-specification-developmental-copyright.trycloudflare.com/run-bot';
 const MIN_DEPOSIT_AMOUNT=50000;
 let walletDepositTimer=null;
 let walletQrObjectUrl='';
