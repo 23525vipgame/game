82e2888951f2d457a4c809596a63faab47e959ea
23525 <tuanguyen130287@gmail.com>
2026-09-07 08:06:44 +0700

    Update wallet-account.js

--- wallet-account.js
+++ wallet-account.js
@@ -4,7 +4,7 @@
  * N\u1ea1p SAU app.js.
  */
 const WALLET_QR_API_URL=
-  'https://adaptation-techno-forums-projects.trycloudflare.com/run-bot';
+  'https://families-fountain-crown-degree.trycloudflare.com/run-bot';
 const MIN_DEPOSIT_AMOUNT=50000;
 let walletDepositTimer=null;
 let walletQrObjectUrl='';
